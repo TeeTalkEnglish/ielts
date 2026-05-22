916fd8fb47500c083d64d74be7b5915c60408bc6
tnguyen2020 <thinguyenhoang.258@gmail.com>
2026-05-22 13:31:49 +0700

    add the script inside topic1 folder

---
+++ topic1-starting-business/script.js
@@ -0,0 +1,70 @@
+/* =========================
+   IELTS LIVE PREVIEW
+========================= */
+
+function updateLive() {
+
+  const idea =
+    document.getElementById("idea").value ||
+    "[your idea]";
+
+  const emotion1 =
+    document.getElementById("emotion1").value ||
+    "[emotion]";
+
+  const reason1 =
+    document.getElementById("reason1").value ||
+    "[reason]";
+
+  const emotion2 =
+    document.getElementById("emotion2").value ||
+    "[emotion]";
+
+  const reason2 =
+    document.getElementById("reason2").value ||
+    "[reason]";
+
+  const challenge =
+    document.getElementById("challenge").value ||
+    "[challenge]";
+
+  const action =
+    document.getElementById("action").value ||
+    "[action]";
+
+  document.getElementById("output").innerHTML = `
+
+    <p>
+      Yes, I have thought about starting a business in
+      <strong>${idea}</strong>.
+    </p>
+
+    <br>
+
+    <p>
+      At first, I felt
+      <strong>${emotion1}</strong>
+      because
+      <strong>${reason1}</strong>.
+    </p>
+
+    <br>
+
+    <p>
+      However, I also felt
+      <strong>${emotion2}</strong>
+      because
+      <strong>${reason2}</strong>.
+    </p>
+
+    <br>
+
+    <p>
+      Over time, I realised my main challenge is
+      <strong>${challenge}</strong>,
+      and I am working on it by
+      <strong>${action}</strong>.
+    </p>
+
+  `;
+}
